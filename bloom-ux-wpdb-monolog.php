b6a56701ec48c36d4351409b87d594fa197632d0
Felipe Lavín Z <felipe@bloom-ux.com>
2022-04-26 19:21:45 -0400

    version bruta

---
+++ bloom-ux-wpdb-monolog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bloom_UX\WPDB_Monolog;
+
+use WordPressHandler\WordPressHandler;
+
+if ( ! defined( 'Inpsyde\Wonolog\LOG' ) ) {
+	return;
+}
+
+$wpdb_handler = new WordPressHandler(
+	null,
+	'log',
+	[ 'username', 'userid' ],
+	\Monolog\Logger::DEBUG
+);
+$wpdb_handler->conf_table_size_limiter( 256000 );
+$record = [ 'extra' => [] ];
+$wpdb_handler->initialize( $record );
+
+Inpsyde\Wonolog\bootstrap(
+	$wpdb_handler
+);
