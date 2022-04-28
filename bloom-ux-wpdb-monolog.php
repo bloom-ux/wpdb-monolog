fd62828c21686307aced2d71e35852b4fbac00b5
Felipe Lavín Z <felipe@bloom-ux.com>
2022-04-28 12:01:47 -0400

    improve initialization and db handler

--- bloom-ux-wpdb-monolog.php
+++ bloom-ux-wpdb-monolog.php
@@ -2,6 +2,7 @@
 
 namespace Bloom_UX\WPDB_Monolog;
 
+use Inpsyde\Wonolog\Configurator;
 use WordPressHandler\WordPressHandler;
 
 if ( ! defined( 'Inpsyde\Wonolog\LOG' ) ) {
@@ -10,14 +11,24 @@ if ( ! defined( 'Inpsyde\Wonolog\LOG' ) ) {
 
 $wpdb_handler = new WordPressHandler(
 	null,
-	'log',
-	[ 'username', 'userid' ],
-	\Monolog\Logger::DEBUG
+	'logs',
+	array(
+		'wp',
+		'username',
+		'userid',
+		'file',
+		'line',
+		'extra'
+	)
 );
-$wpdb_handler->conf_table_size_limiter( 256000 );
+$wpdb_handler->conf_table_size_limiter( 512000 );
 $record = [ 'extra' => [] ];
 $wpdb_handler->initialize( $record );
 
-\Inpsyde\Wonolog\bootstrap(
-	$wpdb_handler
+add_action(
+	'wonolog.setup',
+	function( Configurator $config ) use ( $wpdb_handler ) {
+		$config->disableFallbackHandler();
+		$config->pushHandler( $wpdb_handler );
+	}
 );
